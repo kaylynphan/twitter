6ba832dd4afd02ca1e84320acf95dfa128fba449
Kaylyn Phan <kaylynphan@fb.com>
2022-06-22 16:58:21 -0700

    like, retweet, reply button outlets

--- twitter/TweetCell.m
+++ twitter/TweetCell.m
@@ -15,10 +15,25 @@
     // Initialization code
 }
 
+- (IBAction)didTapReply:(id)sender {
+}
+
+- (IBAction)didTapRetweet:(id)sender {
+}
+
+- (IBAction)didTapFavorite:(id)sender {
+    //self.tweet.favorited = YES;
+    //self.tweet.favoriteCount += 1;
+}
+
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+-(void)refreshData {
+    
 }
 
 @end
